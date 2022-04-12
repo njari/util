795eb50615b9b16bcfc3acd68e604b17d7ba0354
Nubra <nubra@nubra-MacBook-Pro.local>
2022-04-12 22:11:32 +0530

    email changes

--- src/main/java/in/njari/util/UtilApplication.java
+++ src/main/java/in/njari/util/UtilApplication.java
@@ -2,8 +2,10 @@ package in.njari.util;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UtilApplication {
 
 	public static void main(String[] args) {
--- src/main/java/in/njari/util/src/ExceptionReportingAdvice.java
+++ src/main/java/in/njari/util/src/ExceptionReportingAdvice.java
@@ -8,21 +8,42 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
 @Aspect
 @Component
 public class ExceptionReportingAdvice {
+
+    private static final String TO = "to";
+    private static final String CONTENT = "content";
+    private static final String SUBJECT = "subject";
+    public static final Map<String,String> emailMap = new HashMap<>();
+
+    @PostConstruct
+    public void init(){
+        emailMap.put(TO, "nubra.jarial@dealshare.in");
+    }
 
     @Autowired
     ChatServiceClient chatServiceClient;
 
     @Pointcut(value = "bean(*Controller)")
     public void pointcut(){
-
     }
 
     @AfterThrowing(value = "pointcut()", throwing = "ex")
     public void afterThrowing(JoinPoint pjp, Exception ex ) throws Throwable {
-        System.out.println(pjp.getSignature() + " throws : " + ex.getMessage());
+        System.out.println(pjp.getSignature() + ex.getMessage());
 
+        emailMap.put(SUBJECT, ex.getMessage());
+        StringBuilder content = new StringBuilder();
+        for (int i = 0 ; i < ex.getStackTrace().length && i < 10 ;i++ ){
+            content.append(ex.getStackTrace()[i]);
+            content.append("<br/>");
+        }
+        emailMap.put(CONTENT, content.toString());
+        chatServiceClient.sendEmail(emailMap);
     }
 }
